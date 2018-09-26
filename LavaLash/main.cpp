0c01ec25de4172189360aeed29d70a1981493a2b
Dino <dino.budic@gmail.com>
2018-09-26 05:52:18 +0200

    Added fixed fps

--- LavaLash/main.cpp
+++ LavaLash/main.cpp
@@ -14,18 +14,25 @@
 #include "Engine.h"
 
 Engine *engine = nullptr;
-
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
 int main(int argc,  char * argv[]) {
 	// insert code here...
-
+	Uint32 frameStart, frameTime;
 	SDL_SetMainReady();
 	 Engine::Instance()->init("LavaLash", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, false);
 	
 	while (Engine::Instance()->running()) {
+		frameStart = SDL_GetTicks();
+
 		Engine::Instance()->handleEvents();
 		Engine::Instance()->update();
 		Engine::Instance()->render();
-		SDL_Delay(10);
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameTime < DELAY_TIME) {
+			SDL_Delay((int)(DELAY_TIME - frameStart));
+		}
 	}
 	Engine::Instance()->clean();
 	return 0;
